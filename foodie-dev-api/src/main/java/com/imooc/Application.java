d79904db428712b891e9305fdf12c2e409c76b45
1439107348 <1439107348@qq.com>
2020-07-20 16:20:10 +0800

    添加相关的数据库文件

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,11 +2,17 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
 //启动类
 @SpringBootApplication
+//扫描mybatis 通用mapper所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
+//开启事务管理 可加可不加
+@EnableTransactionManagement
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
