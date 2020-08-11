bc55df300f2aa1b6d51635cade8627d5adf45ebf
1439107348 <1439107348@qq.com>
2020-08-11 21:22:36 +0800

    完善定时任务

--- foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -1,6 +1,8 @@
 package com.imooc.controller;
 
+import com.imooc.enums.OrderStatusEnum;
 import com.imooc.enums.PayMethod;
+import com.imooc.pojo.OrderStatus;
 import com.imooc.pojo.bo.SubmitOrderBO;
 import com.imooc.pojo.vo.MerchantOrdersVO;
 import com.imooc.pojo.vo.OrderVO;
@@ -12,11 +14,12 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -29,8 +32,8 @@ public class OrdersController extends BaseController {
 
     @Autowired
     private OrderService orderService;
-
-
+    @Autowired
+    private RestTemplate restTemplate;  //WebMvcConfig添加
 
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("/create")
@@ -61,30 +64,46 @@ public class OrdersController extends BaseController {
        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, "", true);
 
         // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
-//        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
-//        merchantOrdersVO.setReturnUrl(payReturnUrl);
-//
-//        // 为了方便测试购买，所以所有的支付金额都统一改为1分钱
-//        merchantOrdersVO.setAmount(1);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.add("imoocUserId","imooc");
-//        headers.add("password","imooc");
-//
-//        HttpEntity<MerchantOrdersVO> entity =
-//                new HttpEntity<>(merchantOrdersVO, headers);
-//
-//        ResponseEntity<IMOOCJSONResult> responseEntity =
-//                restTemplate.postForEntity(paymentUrl,
-//                        entity,
-//                        IMOOCJSONResult.class);
-//        IMOOCJSONResult paymentResult = responseEntity.getBody();
-//        if (paymentResult.getStatus() != 200) {
-//            logger.error("发送错误：{}", paymentResult.getMsg());
-//            return IMOOCJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
-//        }
+        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
+        merchantOrdersVO.setReturnUrl(payReturnUrl);
+
+        // 为了方便测试购买，所以所有的支付金额都统一改为1分钱
+        merchantOrdersVO.setAmount(1);
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("imoocUserId","imooc");
+        headers.add("password","imooc");
+
+        HttpEntity<MerchantOrdersVO> entity =
+                new HttpEntity<>(merchantOrdersVO, headers);
+
+        ResponseEntity<IMOOCJSONResult> responseEntity =
+                restTemplate.postForEntity(paymentUrl,
+                        entity,
+                        IMOOCJSONResult.class);
+        IMOOCJSONResult paymentResult = responseEntity.getBody();
+        if (paymentResult.getStatus() != 200) {
+            logger.error("发送错误：{}", paymentResult.getMsg());
+            return IMOOCJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
+        }
 
         return IMOOCJSONResult.ok(orderId);
+    }
+
+
+    @PostMapping("notifyMerchantOrderPaid")
+    public Integer notifyMerchantOrderPaid(String merchantOrderId) {
+//        orderService.updateOrderStatus(merchantOrderId, OrderStatusEnum.WAIT_DELIVER.type);
+        return HttpStatus.OK.value();
+    }
+
+    @PostMapping("getPaidOrderInfo")
+    public IMOOCJSONResult getPaidOrderInfo(String orderId) {
+
+//        OrderStatus orderStatus = orderService.queryOrderStatusInfo(orderId);
+//        return IMOOCJSONResult.ok(orderStatus);
+        return IMOOCJSONResult.ok();
     }
 }
--- foodie-dev-service/src/main/java/com/imooc/service/OrderService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.imooc.service;
 
+import com.imooc.pojo.OrderStatus;
 import com.imooc.pojo.bo.SubmitOrderBO;
 import com.imooc.pojo.vo.OrderVO;
 
@@ -9,4 +10,21 @@ public interface OrderService {
      * @param submitOrderBO
      */
     public OrderVO createOrder(SubmitOrderBO submitOrderBO);
+    /**
+     * 修改订单状态
+     * @param orderId
+     * @param orderStatus
+     */
+    public void updateOrderStatus(String orderId, Integer orderStatus);
+    /**
+     * 查询订单状态
+     * @param orderId
+     * @return
+     */
+    public OrderStatus queryOrderStatusInfo(String orderId);
+
+    /**
+     * 关闭超时未支付订单
+     */
+    public void closeOrder();
 }
